8b0bd997ed34e43c748b1471f2575ebabc368052
Gregory Thomas Croisdale <gtcroisdale1@gmail.com>
2017-04-03 14:19:43 -0400

    Initial Commit

---
+++ BaseCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BaseCode
+//
+//  Created by Gregory Thomas Croisdale on 4/3/17.
+//  Copyright © 2017 L&N STEM Academy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
